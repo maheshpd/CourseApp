6ae68445076d6172a7349d2af25d0b3463ce95f7
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-08-04 21:43:01 +0530

    Initial Commit

---
+++ CourseApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CourseApp
+//
+//  Created by Mahesh Prasad on 04/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CourseApp/CourseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseAppApp.swift
+//  CourseApp
+//
+//  Created by Mahesh Prasad on 04/08/21.
+//
+
+import SwiftUI
+
+@main
+struct CourseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
